6b97039bb32fafa740c0e9b35fbc23f9b17931fc
Nguyen Trung Kien <ntkien12@cmc.com.vn>
2018-10-02 13:18:25 +0700

    Upload /files folder

---
+++ web/sites/default/files/php/twig/5bad9ee573dda_page--user--login.html.tw_lZHrfNjsACOIsIJ2cbAekDoyh/fIg_yh1YlaqX3QpIg_Mn0zRgOKmbCFut5jyryksc0uI.php
@@ -0,0 +1,81 @@
+<?php
+
+/* themes/custom/rubicon/templates/page/page--user--login.html.twig */
+class __TwigTemplate_4a6d41316ee8839eae6f155f58c490d0d911617cc3c10ff57ab727041c605e6e extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+            'content' => array($this, 'block_content'),
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("block" => 1);
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array('block'),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 1
+        $this->displayBlock('content', $context, $blocks);
+    }
+
+    public function block_content($context, array $blocks = array())
+    {
+        // line 2
+        echo "  ";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["page"] ?? null), "content", array()), "html", null, true));
+        echo "
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/rubicon/templates/page/page--user--login.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  50 => 2,  44 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("{% block content %}
+  {{ page.content }}
+{% endblock %}
+", "themes/custom/rubicon/templates/page/page--user--login.html.twig", "C:\\xampp\\htdocs\\rubicon-drupal\\web\\themes\\custom\\rubicon\\templates\\page\\page--user--login.html.twig");
+    }
+}
